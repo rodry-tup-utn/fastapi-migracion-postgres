bd6671e3a20d83d9fb677e182af29d7a36a57e48
rodry <rodrymza@gmail.com>
2026-04-13 20:20:22 -0300

    feature: creado archivo main

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from sqlmodel import SQLModel
+from app.core.database import engine
+from app.modules.categoria.model import Categoria
+from app.modules.producto.model import Producto
+from app.modules.producto.router import router as producto_router
+from app.modules.categoria.router import router as categoria_router
+from app.core.errors import NotFoundException
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+
+
+app = FastAPI(lifespan=lifespan, description="Api Basica de Productos y Categorias")
+
+
+@app.exception_handler(NotFoundException)
+def not_found_handler(request, e):
+    return JSONResponse(status_code=404, content={"detail": str(e)})
+
+
+app.include_router(producto_router)
+app.include_router(categoria_router)
