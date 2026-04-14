82d1c0fa39ef1fbcca1a644cb0c328150adbdb89
rodry <rodrymza@gmail.com>
2026-04-14 20:06:42 -0300

    feature: handler para valueError, incluidas rutas de movimientos

--- app/main.py
+++ app/main.py
@@ -5,8 +5,10 @@ from sqlmodel import SQLModel
 from app.core.database import engine
 from app.modules.categoria.model import Categoria
 from app.modules.producto.model import Producto
+from app.modules.movimiento.model import Movimiento
 from app.modules.producto.router import router as producto_router
 from app.modules.categoria.router import router as categoria_router
+from app.modules.movimiento.router import router as movimiento_router
 from app.core.errors import NotFoundException
 
 
@@ -24,5 +26,11 @@ def not_found_handler(request, e):
     return JSONResponse(status_code=404, content={"detail": str(e)})
 
 
+@app.exception_handler(ValueError)
+def value_error_handler(request, e):
+    return JSONResponse(status_code=400, content={"detail": str(e)})
+
+
 app.include_router(producto_router)
 app.include_router(categoria_router)
+app.include_router(movimiento_router)
