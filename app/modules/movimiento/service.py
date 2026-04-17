da9045417b4305aae56207bee6bdc488b9c8ff86
rodry <rodrymza@gmail.com>
2026-04-17 01:10:43 -0300

    feature: funcion para mostrar estadisticas basicas

--- app/modules/movimiento/service.py
+++ app/modules/movimiento/service.py
@@ -1,9 +1,11 @@
 from app.modules.movimiento.model import Movimiento, TIPO_MOVIMIENTO
 from app.core.errors import NotFoundException
-from sqlmodel import Session, select, desc
+from sqlmodel import Session, select, desc, col
 from app.modules.movimiento import schemas
 from app.modules.producto import service as service_producto
 from sqlalchemy.orm import selectinload
+from sqlalchemy import func
+from app.modules.producto.model import Producto
 
 
 def create_movimiento(session: Session, data: schemas.MovimientoCreate):
@@ -72,3 +74,45 @@ def delete(session: Session, id: int):
     session.refresh(movimiento)
 
     return movimiento
+
+
+def get_dashboard_stats(session: Session) -> schemas.DashboardStats:
+
+    # --- 1. ESTADÍSTICAS DE PRODUCTOS (1 sola query) ---
+    # Contamos los IDs (variedad) y sumamos el stock actual
+    query_productos = select(
+        func.count(col(Producto.id)), func.coalesce(func.sum(Producto.stock), 0)
+    ).where(Producto.activo)
+
+    # .first() nos devuelve una tupla con los dos valores
+    variedad_productos, stock_total = session.exec(
+        query_productos
+    ).first()  # type:ignore
+
+    # --- 2. ESTADÍSTICAS DE COMPRAS / ENTRADAS (1 sola query) ---
+    query_compras = select(
+        func.count(col(Movimiento.id)),
+        func.coalesce(func.sum(Movimiento.precio_total), 0.0),
+    ).where(Movimiento.tipo_movimiento == TIPO_MOVIMIENTO.ENTRADA, Movimiento.activo)
+    compras_count, compras_total = session.exec(query_compras).first()  # type:ignore
+
+    # --- 3. ESTADÍSTICAS DE VENTAS / SALIDAS (1 sola query) ---
+    query_ventas = select(
+        func.count(col(Movimiento.id)),
+        func.coalesce(func.sum(Movimiento.precio_total), 0.0),
+    ).where(Movimiento.tipo_movimiento == TIPO_MOVIMIENTO.SALIDA, Movimiento.activo)
+    ventas_count, ventas_total = session.exec(query_ventas).first()  # type:ignore
+
+    # --- 4. ARMADO DEL DASHBOARD ---
+    # Calculamos el balance en memoria, que es una simple resta
+    balance = ventas_total - compras_total
+
+    return schemas.DashboardStats(
+        total_compras_count=compras_count,
+        total_ventas_count=ventas_count,
+        monto_total_ventas=ventas_total,
+        monto_total_compras=compras_total,
+        balance_caja=balance,
+        varidad_productos=variedad_productos,
+        stock_total_actual=stock_total,
+    )
