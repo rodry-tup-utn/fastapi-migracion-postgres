adfcf486ad7b2f07b38f6853a77be72d8103a703
rodry <rodrymza@gmail.com>
2026-05-29 02:25:44 -0300

    refactor: uso de nombres de nuevos esquemas

--- app/modules/categoria/router.py
+++ app/modules/categoria/router.py
@@ -11,7 +11,7 @@ def get_category_service(session: Session = Depends(get_session)) -> CategoriaSe
     return CategoriaService(session)
 
 
-@router.get("/", response_model=list[schemas.CategoriaBaseRead])
+@router.get("/", response_model=list[schemas.CategoriaRead])
 def get_all(
     incluir_inactivos: bool = False,
     svc: CategoriaService = Depends(get_category_service),
@@ -28,7 +28,7 @@ def get_categoria(
     return svc.get_by_id(id, incluir_inactivos)
 
 
-@router.post("/", response_model=schemas.CategoriaBaseRead)
+@router.post("/", response_model=schemas.CategoriaRead)
 def create_categoria(
     data: schemas.CategoriaCreate, svc: CategoriaService = Depends(get_category_service)
 ):
